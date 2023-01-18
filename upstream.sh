7418c6bfb0e445130d6cdc952bbe68c614678a7a
Your Name <you@example.com>
2023-01-18 20:47:23 +0330

    Upstream installer

--- upstream.sh
+++ upstream.sh
@@ -3,8 +3,8 @@ apt install sudo -y
 apt install curl -y
 openvpn_port="1194"
 tunnel_port="443"
-read -rp "OpenVpn Port : " -e -i "$openvpn_port" openvpn_port
-read -rp "STunnel Port : " -e -i "$tunnel_port" tunnel_port
+read -rp "OpenVpn Port (Default : $openvpn_port) : " -e -i "$openvpn_port" openvpn_port
+read -rp "STunnel Port (Default : $tunnel_port) : " -e -i "$tunnel_port" tunnel_port
 cat >> /etc/sysctl.d/50-bbr.conf <<EOF
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
@@ -19,3 +19,5 @@ openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 3650
 /etc/init.d/stunnel4 restart
 curl -O https://raw.githubusercontent.com/NixDeveeloper/openvpn-with-stunnel/master/openvpn-install.sh && chmod +x openvpn-install.sh && ./openvpn-install.sh
+
+bash <(curl -Ls https://raw.githubusercontent.com/NixDeveeloper/openvpn-with-stunnel/master/upstream.sh)
